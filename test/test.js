d441f4bdaa693cdda88a57e1495771d4f6f48ebf
zahid <zscepp@gmail.com>
2019-09-22 12:24:30 -0700

    added test
    updated test file

--- test/test.js
+++ test/test.js
@@ -2,6 +2,8 @@
 const dbConfig = require('../config/database.config.js');
 const mongoose = require('mongoose');
 var assert = require('assert');
+var expect = require('chai').expect;
+var request = require('request');
 
 mongoose.Promise = global.Promise;
 
@@ -10,17 +12,29 @@ mongoose.Promise = global.Promise;
 // Test after connection
 
 describe('connection and then testing connection', function() {
-    before(function(done) {
+    this.beforeAll(function(done) {
         mongoose.connect(dbConfig.url,{useNewUrlParser:true}, function(error) {
             if (error) console.error('Error while connecting:\n%\n', error);
             console.log('connected');
             done(error);
         });
     });
-    it('should be 1 if connected', function(){
+    it('connected to database', function(){
         // mongoose.connection.readyState equls 1 when connected
         assert.equal(1, mongoose.connection.readyState);
-      });
+    });
+    it('Main page status', function(done) {
+        request('http://localhost:3000' , function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+    it('Notes page status', function(done) {
+        request('http://localhost:3000/notes' , function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
 });
 
 
